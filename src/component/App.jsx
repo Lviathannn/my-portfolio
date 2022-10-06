a160d03fdb92014b00436ddadfb88bf9d032d5e2
Lviathannn <valorant.leviathan@gmail.com>
2022-10-06 22:37:33 +0700

    Fix Bug

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,37 +2,17 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Main from "./main/Main";
 import { React, useEffect, useState } from "react";
-import ScrollReveal from "scrollreveal";
 import HashLoader from "react-spinners/HashLoader";
 
 export default function App() {
    const [loading, setLoading] = useState(false);
-   useEffect(() => {
-      const sr = ScrollReveal({
-         origin: "top",
-         distance: "60px",
-         duration: 2500,
-         delay: 150,
-         reset: true,
-      });
-      sr.reveal(`
-         .about__image, .skill__caption, .skill__tools, .skill__card, .contact__caption ,.footer__bio, .footer__info ,.footer__adress, .footer__copy
-      `);
-      sr.reveal(`.home__image, .portfolio__caption, .contact__form`, {
-         origin: `right`,
-      });
-      sr.reveal(`.home__caption, .portfolio__card, .contact__info`, {
-         origin: `left`,
-      });
-      sr.reveal(".about__caption", { origin: "bottom" });
-   }, []);
-
    useEffect(() => {
       setLoading(true);
       setTimeout(() => {
          setLoading(false);
       }, 5000);
    }, []);
+
    return loading ? (
       <div className="h-screen bg-slate-800 flex justify-center items-center">
          <HashLoader
--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./home/Home";
 import About from "./about/About";
 import Skill from "./skill/Skill";
 import Portfolio from "./portfolio/Portfolio";
 import Contact from "./contact/Contact";
+import ScrollReveal from "scrollreveal";
 
 export default function Main() {
+   useEffect(() => {
+      const sr = ScrollReveal({
+         origin: "top",
+         distance: "60px",
+         duration: 2500,
+         delay: 150,
+         reset: true,
+      });
+      sr.reveal(`
+         .about__image, .skill__caption, .skill__tools, .skill__card, .contact__caption ,.footer__bio, .footer__info ,.footer__adress, .footer__copy
+      `);
+      sr.reveal(`.home__image, .portfolio__caption, .contact__form`, {
+         origin: `right`,
+      });
+      sr.reveal(`.home__caption, .portfolio__card, .contact__info`, {
+         origin: `left`,
+      });
+      sr.reveal(".about__caption", { origin: "bottom" });
+   }, []);
+
    return (
       <main className="overflow-x-hidden dark:bg-slate-800 dark:text-slate-50">
          <Home />
